f204eb7170ae6766635aff59dff8c6db33db3c51
Muhammad Bilal <65731147+stubborn-coder@users.noreply.github.com>
2024-02-17 19:53:51 +0500

    added card

--- lib/home.dart
+++ lib/home.dart
@@ -45,45 +45,74 @@ class Homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-            onPressed: () {},
-            icon: const Icon(
-              Icons.menu,
-              semanticLabel: 'menu',
-            )),
-        title: const Text('SHRINE'),
-        actions: <Widget>[
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.search, semanticLabel: 'search'),
-          ),
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(
-              Icons.tune,
-              semanticLabel: 'filter',
+        appBar: AppBar(
+          leading: IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.menu,
+                semanticLabel: 'menu',
+              )),
+          title: const Text('SHRINE'),
+          actions: <Widget>[
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.search, semanticLabel: 'search'),
             ),
-          )
-        ],
-      ),
-      body: mockProducts.isEmpty
-          ? GridView.builder(
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2, childAspectRatio: 8.0 / 9.0),
-              padding: const EdgeInsets.all(16.0),
-              itemCount: mockProducts.length,
-              itemBuilder: (context, index) {
-                return Card();
-              })
-          // GridView.count(
-          //     crossAxisCount: 2,
-          //     padding: const EdgeInsets.all(16.0),
-          //     childAspectRatio: 8.0 / 9.0,
-          //     children: _buildGridCards(context))
-          : const Center(
-              child: Text('No data found'),
-            ),
-    );
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.tune,
+                semanticLabel: 'filter',
+              ),
+            )
+          ],
+        ),
+        body: GridView.builder(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2, childAspectRatio: 8.0 / 9.0),
+            padding: const EdgeInsets.all(16.0),
+            itemCount: mockProducts.length,
+            itemBuilder: (context, index) {
+              return Card(
+                  clipBehavior: Clip.antiAlias,
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        AspectRatio(
+                          aspectRatio: 18.0 / 11.0,
+                          child: Image.asset(
+                            mockProducts[index].imageUrl,
+                            fit: BoxFit.fitWidth,
+                          ),
+                        ),
+                        Expanded(
+                            child: Padding(
+                          padding:
+                              const EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+                          child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Text(
+                                  mockProducts[index].name,
+                                  // style: theme.textTheme.titleLarge
+                                ),
+                                const SizedBox(height: 8.0),
+                                Text(
+                                  mockProducts[index].price,
+                                  // style: theme.textTheme.titleSmall,
+                                ),
+                              ]),
+                        ))
+                      ]));
+            })
+        // GridView.count(
+        //     crossAxisCount: 2,
+        //     padding: const EdgeInsets.all(16.0),
+        //     childAspectRatio: 8.0 / 9.0,
+        //     children: _buildGridCards(context))
+        // : const Center(
+        //     child: Text('No data found'),
+        //   ),
+        );
   }
 }
