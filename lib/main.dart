d96ec71d7b49952caf16505b3d52b21c264caedd
Muhammad Bilal <65731147+stubborn-coder@users.noreply.github.com>
2024-04-26 20:59:34 +0500

    works

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,7 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:testproject/provider/user_provider.dart';
-
+import "package:flutter/material.dart";
 
 void main() {
-  runApp(const ProviderScope(child: MyApp()));
-
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -14,31 +10,94 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
-        title: 'Riverpod',
-        theme: ThemeData(
-          useMaterial3: true,
-        ),
-        home:const Home(),
-      );
-
+      title: 'Riverpod',
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(
+        title: 'login',
+      ),
+    );
   }
 }
 
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
 
-class Home extends ConsumerWidget {
-  const Home({super.key});
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final usersNotifier = ref.watch(userProvider);
-   return switch (usersNotifier) {
-      AsyncData(:final value) => Padding(padding: const EdgeInsets.all(8.0),
-      child: ListView.separated(itemBuilder: (cxt,i)=>Card(child: ListTile(title: Text(value[i].name ?? ''),),), separatorBuilder: (cxt,i)=> Divider(), itemCount: value.length),),
-      AsyncError(:final error) => Text('error: $error'),
-      _ => const Center(child:  CircularProgressIndicator(),),
-    };
-}
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
-
+class _MyHomePageState extends State<MyHomePage> {
+  bool _show = true;
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+          child: Center(
+              child: Padding(
+        padding: const EdgeInsets.only(top: 0.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            const Padding(
+              padding: EdgeInsets.only(bottom: 12.0, left: 10.0, right: 10.0),
+              child: TextField(
+                  obscureText: false,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Email',
+                  )),
+            ),
+            Padding(
+                padding: EdgeInsets.only(bottom: 90.0, left: 10.0, right: 10.0),
+                child: TextField(
+                    obscureText: _show,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Password',
+                        suffixIcon: IconButton(
+                          icon: const Icon(Icons.lock),
+                          onPressed: () {
+                            _show = !_show;
+                          },
+                        )))),
+            const Text("New user? Create Account"),
+            TextButton(
+              style: TextButton.styleFrom(
+                textStyle: const TextStyle(fontSize: 14),
+              ),
+              onPressed: () {},
+              child: const Text('Forgot Password?'),
+            )
+          ],
+        ),
+      ))),
+      floatingActionButton: FloatingActionButton.extended(
+          onPressed: () {}, label: const Text("Login")),
+    );
+  }
+}
