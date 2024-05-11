d81ad00bb969fc9a36c2b24d9d6360ca93d2d66d
Muhammad Bilal <65731147+mbilal0612@users.noreply.github.com>
2024-05-11 15:25:55 +0500

    test

--- lib/main.dart
+++ lib/main.dart
@@ -1,74 +1,15 @@
-import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:testproject/provider/todo_provider.dart';
+class Area {
+  static double get pi => 3.141592;
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => TodoProvider(),
-      child: MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
-        ),
-        home: const MyHomePage(title: 'Flutter Demo Home Page'),
-      ),
-    );
+  double circle(double radius) {
+    return pi * radius.square;
   }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  void initState() {
-    super.initState();
-    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
-      Provider.of<TodoProvider>(context, listen: false).getAllTodos();
-    });
+  double rectangle(double length, double width) {
+    return length * width;
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: Text(widget.title),
-        ),
-        body: Consumer<TodoProvider>(builder: (context, value, child) {
-          if (value.isLoading) {
-            return const Center(child: CircularProgressIndicator());
-          }
+}
 
-          final todos = value.todos;
-          return ListView.builder(
-              itemCount: todos.length,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  leading: CircleAvatar(
-                    child: Text((index + 1).toString()),
-                  ),
-                  title: Text(todos[index].title),
-                  trailing:
-                      todos[index].completed ? const Icon(Icons.done) : null,
-                );
-              });
-        }));
-  }
+extension NumSquare on num{
+  num get square => this* this;
 }
---
+++ test/unit/area_test.dart
@@ -0,0 +1,23 @@
+import "package:testproject/main.dart";
+
+void main() {
+  test("Area of circle with radius 1 should be 3.141592", () { 
+    
+    //Arrange
+    Area area = Area();
+    //Act
+    double result = area.circle(1);
+    //Assert
+    expect(result,3.141592);
+
+  });
+
+  test("Area of th rectangle length 2 and width 1 ", ( ) {
+    //Arrange 
+    Area area = Area();
+    //Act
+    double result = area.rectanlge(2,1);
+    //Assert
+    expect(result,2);
+  });
+}
